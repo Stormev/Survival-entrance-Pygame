6cf4a7dc4e662daa027431e322f05548946cc196
Stormev <pidala1990@gmail.com>
2024-01-03 15:13:41 +0300

    game_update

--- main.py
+++ main.py
@@ -1,1 +1,59 @@
 import pygame
+import sys
+import os
+
+FPS = 50
+pygame.init()
+WIDTH, HEIGHT = size = 1024, 512
+screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def start_screen():
+    intro_text = ["Управление:", 'A - Движение налево', 'D - Движение направо']
+    fon = 'images/start_screen_controll.png'
+    fon = pygame.transform.scale(load_image(fon), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 120, 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('Black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord = text_coord[0], text_coord[1] + 10
+        intro_rect.top = text_coord[1]
+        intro_rect.x = text_coord[0]
+        text_coord = text_coord[0], text_coord[1] + intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    clock = pygame.time.Clock()
+
+    tip = font.render('Нажмите TAB чтобы начать игру', 1, pygame.Color('Black'))
+    tip_rect = tip.get_rect()
+    tip_rect.x = text_coord[0]
+    tip_rect.y = 430
+    screen.blit(tip, tip_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_TAB:
+                    return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+start_screen()
