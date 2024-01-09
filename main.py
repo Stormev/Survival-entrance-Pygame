328a404f67639dd1a307f51caa461a8f220ebb08
Stormev <pidala1990@gmail.com>
2024-01-09 16:14:42 +0300

    game_update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import sqlite3
 import sys
 import os
 
@@ -15,12 +16,16 @@ screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 
-def out():
+database = sqlite3.connect('data/scores.db')
+cursor = database.cursor()
+
+
+def out():  # Выход
     pygame.quit()
     sys.exit()
 
 
-def load_image(name):
+def load_image(name):  # Загрузка картинки
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"'{fullname}' не найден")
@@ -29,11 +34,11 @@ def load_image(name):
     return image
 
 
-Locations = [load_image('images/background_1.png'), load_image('images/background_2.png'),
-             load_image('images/background_3.png')]
+Locations = [load_image('images/background_3.png'), load_image('images/background_1.png'),
+             load_image('images/background_2.png')]
 
 
-def start_screen():
+def start_screen(): # Стартовый экран
     intro_text = ["Управление:", 'A - Движение налево', 'D - Движение направо']
     fon = 'images/start_screen_controll.png'
     fon = pygame.transform.scale(load_image(fon), (WIDTH, HEIGHT))
@@ -69,12 +74,18 @@ start_screen()
 player_group = pygame.sprite.Group()
 background_group = pygame.sprite.Group()
 
+SCORE = 0
+TIME_LIFE = 0
 
-class Player(pygame.sprite.Sprite):
+
+class Player(pygame.sprite.Sprite): # Персонаж
     def __init__(self, pos_x):
         super().__init__(player_group)
         self.pos_y = HEIGHT - 155
         self.speed = 8
+
+        self.hungry = 100
+        self.temp = 10
 
         self.Rframes = []
         self.Lframes = []
@@ -87,7 +98,7 @@ class Player(pygame.sprite.Sprite):
         self.image = self.frames_static[0]
         self.rect = self.rect.move(pos_x, self.pos_y)
 
-    def move(self, side): # True = right, False = Left
+    def move(self, side):  # True = right, False = Left ДВИЖЕНИЕ
         if side:
             self.rect = self.rect.move(self.speed, 0)
 
@@ -102,7 +113,7 @@ class Player(pygame.sprite.Sprite):
     def static(self):
         self.image = self.frames_static[0]
 
-    def cut_sheet(self, sheet, columns, rows):
+    def cut_sheet(self, sheet, columns, rows):  # Нарезка
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
         for j in range(rows):
@@ -113,13 +124,51 @@ class Player(pygame.sprite.Sprite):
 
 
 def load_location(cur_id):
-    if cur_id < len(background_group) or cur_id >= 0:
+    if 0 <= cur_id < len(Locations):
         background = Locations[cur_id]
         screen.blit(background, (0, 0))
 
 
-LOCATION_NOW = 0
+LOCATION_NOW = len(Locations) // 2
+
+
+def next_locations(turn):  # True = right False = left ЗАГРУЗКА ЛОКАЦИИ
+    global LOCATION_NOW
+    if turn and len(Locations) > LOCATION_NOW + 1:
+        LOCATION_NOW += 1
+        player.rect.x = 100
+    elif LOCATION_NOW - 1 >= 0 and not turn:
+        LOCATION_NOW -= 1
+        player.rect.x = WIDTH - 150
+    elif turn:
+        player.rect.x -= 20
+    else:
+
+        player.rect.x += 20
+
+
 player = Player(WIDTH // 2.5)
+
+
+def end_game(status):  # False = lose  True = wib
+    global player
+    player = None
+
+    screen.fill((50, 50, 70))
+    fon = 'images/start_screen_controll.png'
+    fon = pygame.transform.scale(load_image(fon), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+
+    while True:
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                out()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+hungry_count = 0
+
 # main cycle
 while True:
     for event in pygame.event.get():
@@ -130,11 +179,30 @@ while True:
             player.move(True)
         elif keys[K_a]:
             player.move(False)
+    plr_pos = player.rect.x
+    if plr_pos < 50:
+        next_locations(False)
+    elif plr_pos > WIDTH - 50:
+        next_locations(True)
+
+    if hungry_count > 5:
+        hungry_count = 0
+        player.hungry -= 1
+    else:
+        hungry_count += 1
+
+    if player.hungry < 0:
+        end_game(False)
+        break
 
     screen.fill((50, 50, 70))
     load_location(LOCATION_NOW)
     player_group.update()
     player_group.draw(screen)
+
+    print('--------------------------')
+    print('Hungry:', player.hungry)
+    print('Temperature:', player.temp)
 
     pygame.display.flip()
     clock.tick(FPS)
